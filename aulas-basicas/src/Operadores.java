7559a490323234b24846e4a14a9c3dbeda84255d
dev-MichaelCruz <michaelcruz.dev@gmail.com>
2023-09-20 00:51:31 -0300

    Operadores

---
+++ aulas-basicas/src/Operadores.java
@@ -0,0 +1,66 @@
+public class Operadores {
+    public static void main(String[] args) {
+        
+        double soma = 12.7 + 32.93;
+        int subtracao =  3 - 2;
+        int multiplicacao = 3 * 3;
+        int divisao = 4 / 2;
+        int modulo = 18 % 2; //resto da divisao
+        double resultado = (10*7) + (20/4);
+
+        
+        String concatenacao ="?"; 
+        
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao); //31
+        
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao); //1111
+        
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao); //1111
+        
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao); //1111
+        
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao); //13
+
+
+
+        //Invertendo o valor de uma variável de positivo para negativo
+        int numero = 5;
+
+        numero = -numero;
+        System.out.println(numero); //-5
+        
+        //Invertando de negativo para positivo
+        int numero2 = -5;
+        numero2 = numero2 * -1;
+        System.out.println(numero2); //5
+
+
+        //CONDICIONAIS
+        int a = 9;
+        int b = 8;
+    
+        //if else
+        if(a == b){
+            System.out.println("verdadeiro");
+        } else {
+            System.out.println("falso");
+        }
+
+        //if else (clean code)
+        if(a != b){
+            System.out.println("verdadeiro");
+            
+        }
+        
+        //Operador ternário
+        //se a igual a b, entao "verdadeiro" senao "falso"
+        String result = a == b ? "verdadeiro" : "falso";
+        System.out.println(result);
+
+    }
+}
--- aulas-basicas/src/TiposVariaveis.java
+++ aulas-basicas/src/TiposVariaveis.java
@@ -2,12 +2,26 @@ public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         
         String meuNome = "Michael Cruz";
-
+        System.out.println(meuNome);
+        
+        char sexo = 'M';
+        System.out.println(sexo);
+        
         double salario = 2000;
+        System.out.println(salario);
+        
         float numeroQuebrado = 2.80f;
-        double numeroQUebradoMaisPreciso = 3.91564;
+        System.out.println(numeroQuebrado);
+        
+        double numeroQuebradoMaisPreciso = 3.91564;
+        System.out.println(numeroQuebradoMaisPreciso);
+        
         short numerCurto = 1;
+        System.out.println(numerCurto);
+        
         int numeroNormal = 125;
+        System.out.println(numeroNormal);
+        
 
         //mudando valor de uma variável
         int numero = 5;
