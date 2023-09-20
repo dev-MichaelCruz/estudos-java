ee1261fcd5c0f8f1d539ffb275b2b65080cc6d82
dev-MichaelCruz <michaelcruz.dev@gmail.com>
2023-09-19 23:26:29 -0300

    Tipos mais comund de variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String meuNome = "Michael Cruz";
+
+        double salario = 2000;
+        float numeroQuebrado = 2.80f;
+        double numeroQUebradoMaisPreciso = 3.91564;
+        short numerCurto = 1;
+        int numeroNormal = 125;
+
+        //mudando valor de uma variável
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+        
     }
 }
