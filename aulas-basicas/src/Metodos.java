f66fdf480cf047d87537ebe8cec8009b699d799b
dev-MichaelCruz <michaelcruz.dev@gmail.com>
2023-09-25 20:04:17 -0300

    Aulas sobre métodos

---
+++ aulas-basicas/src/Metodos.java
@@ -0,0 +1,14 @@
+
+public class Metodos {
+
+    /*
+     * Funções, ações ou subrotinas dentro das classes
+     * 
+     * CONVENÇÕES
+     * Deve ser nomeado como verbo.
+     * Seguir camelCase
+     * Todos os métodos devem ser definidos dentro de uma classe
+     * Precisa retornar algum valor, caso não volte, será indicado pela palavra void
+     */
+
+}
