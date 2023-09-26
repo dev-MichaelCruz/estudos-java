cba6cd0ca2f9b0adfdff27cab45a2ec4838ea3cb
dev-MichaelCruz <michaelcruz.dev@gmail.com>
2023-09-25 21:24:16 -0300

    Adicionado arquivo com palavras reservadas

---
+++ aulas-basicas/src/Escopo.java
@@ -0,0 +1,14 @@
+public class Escopo {
+    /*
+     * TIPOS DE ESCOPO
+     * 
+     * - Escopo de Classe
+     * - Escopo de Método
+     * - Escopo de Fluxo
+     */
+
+    /*
+     * Ambiente onde uma variável pode ser acessada
+     */
+
+}
---
+++ aulas-basicas/src/PalavrasReservadas.java
@@ -0,0 +1,89 @@
+public class PalavrasReservadas {
+    /*
+     * CONTROLE DE PACOTES
+     * import
+     * package
+     */
+
+    /*
+     * MODIFICADORES DE ACESSO
+     * public
+     * private
+     * protected
+     */
+
+    /*
+     * PRIMITIVOS
+     * boolean
+     * byte
+     * char
+     * double
+     * float
+     * int
+     * long
+     * short
+     * void
+     */
+
+    /*
+     * MODIFICADORES DE CLASSES, VARIÁVEIS OU MÉTODOS
+     * abstract
+     * class
+     * extends
+     * final
+     * implements
+     * interface
+     * native
+     * new
+     * static
+     * strictfp
+     * synchronized
+     * transient
+     * volatile
+     */
+
+    /*
+     * CONTROLE DE FLUXO DENTRO DE UM BLOCO DE CÓDIGO
+     * break
+     * case
+     * continue
+     * default
+     * do
+     * else
+     * for
+     * if
+     * instanceof
+     * return
+     * switch
+     * while
+     */
+
+    /*
+     * TRATAMENTO DE ERROS
+     * assert
+     * catch
+     * finally
+     * throw
+     * throws
+     * try
+     */
+
+    /*
+     * VARIÁVEIS DE REFERÊNCIA
+     * super
+     * this
+     */
+
+    /*
+     * PALAVRAS RESERVADAS NÃO UTILIZADAS
+     * const
+     * goto
+     */
+
+    /*
+     * LITERAIS RESERVADOS
+     * null
+     * true
+     * false
+     */
+}
